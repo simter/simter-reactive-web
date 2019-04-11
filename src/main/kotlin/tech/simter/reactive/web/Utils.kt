eb7a64e3d47a18680a1064a9ed41eef1eae3b4af
RJ <rongjihuang@gmail.com>
2019-04-11 20:44:27 +0800

    Add TEXT_HTML_UTF8 and TEXT_XML_UTF8

--- src/main/kotlin/tech/simter/reactive/web/Utils.kt
+++ src/main/kotlin/tech/simter/reactive/web/Utils.kt
@@ -1,7 +1,7 @@
 package tech.simter.reactive.web
 
 import org.springframework.http.MediaType
-import org.springframework.http.MediaType.TEXT_PLAIN_VALUE
+import org.springframework.http.MediaType.*
 
 /**
  * @author RJ
@@ -9,4 +9,8 @@ import org.springframework.http.MediaType.TEXT_PLAIN_VALUE
 object Utils {
   const val TEXT_PLAIN_UTF8_VALUE: String = "$TEXT_PLAIN_VALUE;charset=UTF-8"
   val TEXT_PLAIN_UTF8: MediaType = MediaType.valueOf(TEXT_PLAIN_UTF8_VALUE)
+  const val TEXT_HTML_UTF8_VALUE: String = "$TEXT_HTML_VALUE;charset=UTF-8"
+  val TEXT_HTML_UTF8: MediaType = MediaType.valueOf(TEXT_HTML_UTF8_VALUE)
+  const val TEXT_XML_UTF8_VALUE: String = "$TEXT_XML_VALUE;charset=UTF-8"
+  val TEXT_XML_UTF8: MediaType = MediaType.valueOf(TEXT_XML_UTF8_VALUE)
 }
